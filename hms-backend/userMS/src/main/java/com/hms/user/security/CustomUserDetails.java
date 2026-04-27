4f7e565270340dfadaf013fc0def5c1662428752
Gourav Mahor <gourav.mahor@bpaassolutions.com>
2026-04-27 11:58:08 +0530

    eureka client configure in userMs service

--- hms-backend/userMS/src/main/java/com/hms/user/security/CustomUserDetails.java
+++ hms-backend/userMS/src/main/java/com/hms/user/security/CustomUserDetails.java
@@ -23,4 +23,24 @@ public class CustomUserDetails implements UserDetails {
     public Collection<? extends GrantedAuthority> authorities;
 
     private Long profileId;
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
