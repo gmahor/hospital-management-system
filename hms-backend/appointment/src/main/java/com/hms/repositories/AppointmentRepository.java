1959365177066fe0e152216ac4cc553d3c6bf868
gmahor <gouravmahor60@gmail.com>
2026-05-29 17:29:17 +0530

    issue fixed

--- hms-backend/appointment/src/main/java/com/hms/repositories/AppointmentRepository.java
+++ hms-backend/appointment/src/main/java/com/hms/repositories/AppointmentRepository.java
@@ -12,11 +12,15 @@ import java.util.List;
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 
-    @Query(value = "select a.patient_id, p.name as patient_name,p.email as patient_email,p.phone as patient_phone, a.doctor_id,d.name as doctor_name,d.email as doctor_email,d.phone as doctor_phone,a.id as appointment_id, a.appointment_time, a.reason, a.notes, a.status \n" +
-            "from appointment a\n" +
-            "         inner join patients p on p.id = a.patient_id\n" +
-            "        inner join doctors d on d.id = a.doctor_id where a.patient_id = :patientId",
-    nativeQuery = true)
+    @Query(value = "select a.patient_id as patientId, " +
+            "p.name as patientName, p.email as patientEmail, p.phone as patientPhone, " +
+            "a.doctor_id as doctorId, d.name as doctorName, d.email as doctorEmail, d.phone as doctorPhone, " +
+            "a.id as appointmentId, a.appointment_time as appointmentTime, a.reason as reason, a.notes as notes, a.status as status " +
+            "from appointment a " +
+            "inner join patients p on p.id = a.patient_id " +
+            "inner join doctors d on d.id = a.doctor_id " +
+            "where a.patient_id = :patientId",
+            nativeQuery = true)
     List<AppointmentDetails> getAllDetailsByPatientId(@Param("patientId") Long patientId);
 
 
